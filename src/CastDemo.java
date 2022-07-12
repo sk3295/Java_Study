239d188eb7c1ffe2b54cb47679e14ddd246fa6d9
Yun_sk3295 <skltepone@gmail.com>
2022-07-12 21:09:00 +0900

    Initial commit

---
+++ src/CastDemo.java
@@ -0,0 +1,21 @@
+public class CastDemo {
+    public static void main(String[] args) {
+        int i;
+        double d;
+        byte b;
+
+        i = 7/4;
+        System.out.println(i);
+        d = 7/4;
+        System.out.println(d);
+        d = 7/(double)4;
+        System.out.println(d);
+
+        // i = 7 / (double) 4; 타입 불일치 오류 발생
+
+        i = 300;
+        if (i < Byte.MIN_VALUE || i > Byte.MAX_VALUE)
+            System.out.println("byte 타입으로 변환 할 수 없습니다.");
+        else b = (byte)i;
+    }
+}
---
+++ src/CompLogicDemo.java
@@ -0,0 +1,18 @@
+public class CompLogicDemo {
+    public static void main(String[] args) {
+        int x = 0, y = 1;
+        System.out.println((x < 1) || (y-- < 1));
+        System.out.println("x = " + x + ", y = " + y);
+
+        x = 0;
+        y = 1;
+        System.out.println((x < 1) | (y-- < 1));
+        System.out.println("x = " + x + ", y = " + y);
+
+        // < 비트 - 시프트 연산자 >
+        // & : 두 비트가 모두 1일 때만 1이며 나머지는 모두 0이다.
+        // | : 두 비트가 모두 0일 때만 0이며, 나머지는 모두 1이다.
+        // ^ : 두 비트가 서로 다를 떄는 1, 동일할 때는 0이다.
+        // ~ : 1을 0으로, 0을 1로 바꾼다.
+    }
+}
