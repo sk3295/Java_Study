3759dedb33ed32f3647d0c54be6d0e82d23df427
Yun_sk3295 <skltepone@gmail.com>
2022-07-13 07:45:28 +0900

    If

---
+++ src/SImpleIfDemo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SImpleIfDemo {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("숫자를 입력하세요 : ");
+        int number = in.nextInt();
+
+        if (number % 2 == 0)
+            System.out.println("짝수!");
+        else
+            System.out.println("홀수!");
+    }
+}
