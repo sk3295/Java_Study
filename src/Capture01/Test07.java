e93635291819b07982e80a0e300e1592fc9f57c5
Yun_sk3295 <skltepone@gmail.com>
2022-07-15 11:03:05 +0900

    문제

--- src/Capture01/Test07.java
+++ src/Capture01/Test07.java
@@ -1,4 +1,22 @@
 package Capture01;
+import java.util.Scanner;
 
 public class Test07 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        if (a >= 70) {
+            if (b >= 30 && c >= 30) {
+                if (b >= 80 || c >= 80) {
+                    if (a + b + c >= 140) {
+                        System.out.println("졸업 가능");
+                    }
+                }
+            }
+        }
+    }
 }
